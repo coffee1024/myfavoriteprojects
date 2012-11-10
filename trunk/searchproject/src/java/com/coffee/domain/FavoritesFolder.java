3fdbc8194a8d4d7bb5f5b7faaf93304b1fdf15b3
liu.guangqiang1016@gmail.com <liu.guangqiang1016@gmail.com@37a2d4cd-442a-eb95-b57e-407eed79dbfd>
2012-11-10 05:31:53 +0000

    添加ansj分词相关的内容

--- trunk/searchproject/src/java/com/coffee/domain/FavoritesFolder.java
+++ trunk/searchproject/src/java/com/coffee/domain/FavoritesFolder.java
@@ -6,16 +6,14 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import net.paoding.analysis.analyzer.PaodingAnalyzer;
 
+import org.ansj.lucene3.AnsjAnalysis;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.SelectBeforeUpdate;
 import org.hibernate.search.annotations.Analyze;
 import org.hibernate.search.annotations.Analyzer;
@@ -51,7 +49,7 @@ public class FavoritesFolder implements Serializable{
 	@Column(name = "ID")
 	@DocumentId
 	private Long id;
-	@Field(name="content",store= Store.YES,index =Index.YES,analyze=Analyze.YES,analyzer=@Analyzer(impl = PaodingAnalyzer.class ))
+	@Field(name="content",store= Store.YES,index =Index.YES,analyze=Analyze.YES,analyzer=@Analyzer(impl = AnsjAnalysis.class ))
 	@Column(name="CONTENT")
 	private String content;
 
