1802509682c20569c98f02c751090d48bc267c3b
liu.guangqiang1016@gmail.com <liu.guangqiang1016@gmail.com@37a2d4cd-442a-eb95-b57e-407eed79dbfd>
2014-03-17 10:12:32 +0000

    集成apache ftpserver

--- trunk/photo/src/main/java/com/coffee/photo/web/account/RegisterController.java
+++ trunk/photo/src/main/java/com/coffee/photo/web/account/RegisterController.java
@@ -1,7 +1,18 @@
 package com.coffee.photo.web.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.ftpserver.ftplet.Authority;
+import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.ftplet.UserManager;
+import org.apache.ftpserver.impl.DefaultFtpServer;
+import org.apache.ftpserver.usermanager.UserFactory;
+import org.apache.ftpserver.usermanager.impl.WritePermission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +23,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.coffee.photo.entity.account.User;
 import com.coffee.photo.service.account.UserService;
+import com.coffee.photo.utils.ftp.FtpServerListener;
 
 /**
  * 用户注册的Controller.
@@ -31,8 +43,27 @@ public class RegisterController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String register(@Valid User user, RedirectAttributes redirectAttributes) {
+	public String register(@Valid User user, RedirectAttributes redirectAttributes,HttpServletRequest request) throws FtpException {
 		userService.registerUser(user);
+		 DefaultFtpServer server = (DefaultFtpServer) request.getServletContext().getAttribute(FtpServerListener.FTPSERVER_CONTEXT_NAME);
+		         if(server!=null){
+		             UserManager um = server.getUserManager();
+		             if(um.doesExist(user.getLoginName())){
+		                 um.delete(user.getLoginName());
+		             }
+		             UserFactory userFact = new UserFactory();
+		             userFact.setName(user.getLoginName());
+		             userFact.setPassword(user.getPlainPassword());
+		             userFact.setHomeDirectory("D:\\tmp\\"+user.getLoginName());
+		             userFact.setMaxIdleTime(600000);//10分钟无操作自动断开连接
+		             List<Authority> alist = new ArrayList<Authority>();
+		             Authority a = new WritePermission();//写权限
+		             alist.add(a);
+		             userFact.setAuthorities(alist);
+		             org.apache.ftpserver.ftplet.User ftpUser = userFact.createUser();
+		             um.save(ftpUser);
+		         }
+
 		redirectAttributes.addFlashAttribute("username", user.getLoginName());
 		return "redirect:/login";
 	}
